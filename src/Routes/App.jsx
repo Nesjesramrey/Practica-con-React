3f218b601c108047fb9ee13515753b2e7aea2067
Nestor <nesjesramrey2@gmail.com>
2022-05-12 18:27:48 -0500

    add styles

--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
-import PasswordRecovery from '../pages/PasswordRecovery';
+import RecoveryPassword from '../pages/RecoveryPassword';
 import SendEmail from '../pages/SendEmail';
 import NewPassword from '../pages/NewPassword';
 import MyAccount from '../pages/MyAccount';
@@ -22,12 +22,12 @@ const App = () => {
 					<Route exact path="/" element={<Home />} />
 					<Route exact path="/login" element={<Login/>} />
 					<Route exact path="/recovery-password" element={<RecoveryPassword />} />
-					<Route exact path="/recovery-password" element={<SendEmail />} />
-					<Route exact path="/recovery-password" element={<NewPassword />} />
-					<Route exact path="/recovery-password" element={<MyAccount />} />
-					<Route exact path="/recovery-password" element={<CreateAccount />} />
-					<Route exact path="/recovery-password" element={<Checkout />} />
-					<Route exact path="/recovery-password" element={<Orders />} />
+					<Route exact path="/send-email" element={<SendEmail />} />
+					<Route exact path="/new-password" element={<NewPassword />} />
+					<Route exact path="/my-account" element={<MyAccount />} />
+					<Route exact path="/singup" element={<CreateAccount />} />
+					<Route exact path="/checkout" element={<Checkout />} />
+					<Route exact path="/orders" element={<Orders />} />
 					<Route path='*' element={<NotFound />} />
 				</Routes>
 			</Layout>
