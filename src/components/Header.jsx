c588cda2cbfe0ccdc35e1f786cff34050f2a4404
Nestor <nesjesramrey2@gmail.com>
2022-05-12 19:15:26 -0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import "../styles/Header.scss"
 const Header = () => {
   return (
     <nav>
-      <img src="./icons/icon_menu.svg" alt="menu" class="menu" />
+      <img src="./icons/icon_menu.svg" alt="menu" className="menu" />
 
       <div className="navbar-left">
         <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
